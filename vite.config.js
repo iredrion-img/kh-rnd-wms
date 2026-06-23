23cf82a6c20908287204183b2468f2a351e30e03
hooooni46-cell <hooooni46@gmail.com>
2026-06-23 14:14:52 +0900

    Fix Vite Host Error for Localtunnel

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: ['jesenia-indiscrete-laraine.ngrok-free.dev'],
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
